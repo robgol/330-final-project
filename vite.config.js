377874aea43f55c6e4fdcc089346916d76e0e822
Robson Correia <rmvcorreia@gmail.com>
2026-06-04 00:48:46 -0100

    Add `gh-pages` integration for deployment scripts

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
-        tailwindcss(),
+        tailwindcss()
     ],
-    // Substitui pelo nome exato do teu repositório no GitHub entre barras
-    base: '/330-final-project/',
+    base: '/330-final-project/'
 });
